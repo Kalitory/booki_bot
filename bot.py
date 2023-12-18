2f8ec889a1e065397892a020bfb606de8a0bfeef
Kalitory <vikakalistratova7926@gmail.com>
2023-12-18 18:46:39 +0300

    Супермегакрутое изменение ёу

--- bot.py
+++ bot.py
@@ -1,12 +1,15 @@
+import asyncio
 from aiogram import Bot, Dispatcher
 from environs import Env
 
 from hendlers import router
 
 
-# Создаем глобальный объект бота
+# чтение окружения
 env = Env()
 env.read_env()
+
+# Создаем глобальный объект бота
 bot = Bot(token=env('BOT_TOKEN'), parse_mode='HTML')
 dp = Dispatcher()
 
@@ -19,3 +22,7 @@ async def tg_bot_main():
     # Пропускаем накопившиеся апдейты и запускаем polling
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(tg_bot_main())
